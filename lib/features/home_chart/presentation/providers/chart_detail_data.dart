77e9d90196217bdb5dfc58a2044e551a11a9ba5b
Rizal Anggoro <gnoogler4@gmail.com>
2024-02-16 15:33:50 +0700

    refactor: chart detail data: future -> async notifier

--- lib/features/home_chart/presentation/providers/chart_detail_data.dart
+++ lib/features/home_chart/presentation/providers/chart_detail_data.dart
@@ -12,69 +12,77 @@ import '../../../../shared/presentation/providers/selected_wallet.dart';
 import '../../domain/entities/chart_detail_data.dart';
 import 'selected_chart_detail_date.dart';
 
-final chartDetailDataProvider = FutureProvider<ChartDetailData?>((ref) async {
-  final isar = ref.watch(isarSourceProvider).instance;
-  final walletId = ref.watch(selectedWalletProvider).value?.id;
-  final dateRangeFilter = ref.watch(selectedDateRangeFilterProvider);
-  final date = ref.watch(selectedChartDetailDateProvider);
+class ChartDetailDataNotifier extends AsyncNotifier<ChartDetailData?> {
+  @override
+  Future<ChartDetailData?> build() async {
+    final isar = ref.watch(isarSourceProvider).instance;
+    final walletId = ref.watch(selectedWalletProvider).value?.id;
+    final dateRangeFilter = ref.watch(selectedDateRangeFilterProvider);
+    final date = ref.watch(selectedChartDetailDateProvider);
 
-  if (date != null && walletId != null) {
-    final nextDate = switch (dateRangeFilter) {
-      DateRangeFilter.yearly => DateTime(
-          date.year + 1,
-        ),
-      DateRangeFilter.monthly => DateTime(
-          date.year,
-          date.month + 1,
-        ),
-      DateRangeFilter.daily => DateTime(
-          date.year,
-          date.month,
-          date.day + 1,
-        ),
-    };
+    if (date != null && walletId != null) {
+      final nextDate = switch (dateRangeFilter) {
+        DateRangeFilter.yearly => DateTime(
+            date.year + 1,
+          ),
+        DateRangeFilter.monthly => DateTime(
+            date.year,
+            date.month + 1,
+          ),
+        DateRangeFilter.daily => DateTime(
+            date.year,
+            date.month,
+            date.day + 1,
+          ),
+      };
 
-    // await Future.delayed(const Duration(seconds: 2));
+      // await Future.delayed(const Duration(seconds: 2));
 
-    return ChartDetailData(
-      dateTime: date,
-      transactions: await isar.transactionModels
-          .filter()
-          .wallet((q) => q.idEqualTo(walletId))
-          .dateBetween(
-            date,
-            nextDate,
-            includeLower: true,
-            includeUpper: false,
-          )
-          .sortByDateDesc()
-          .findAll(),
-      totalIncome: await isar.transactionModels
-          .filter()
-          .wallet((q) => q.idEqualTo(walletId))
-          .dateBetween(
-            date,
-            nextDate,
-            includeLower: true,
-            includeUpper: false,
-          )
-          .category((q) => q.typeEqualTo(CategoryType.income))
-          .amountProperty()
-          .sum(),
-      totalExpense: await isar.transactionModels
-          .filter()
-          .wallet((q) => q.idEqualTo(walletId))
-          .dateBetween(
-            date,
-            nextDate,
-            includeLower: true,
-            includeUpper: false,
-          )
-          .category((q) => q.typeEqualTo(CategoryType.expense))
-          .amountProperty()
-          .sum(),
-    );
+      return ChartDetailData(
+        dateTime: date,
+        transactions: await isar.transactionModels
+            .filter()
+            .wallet((q) => q.idEqualTo(walletId))
+            .dateBetween(
+              date,
+              nextDate,
+              includeLower: true,
+              includeUpper: false,
+            )
+            .sortByDateDesc()
+            .findAll(),
+        totalIncome: await isar.transactionModels
+            .filter()
+            .wallet((q) => q.idEqualTo(walletId))
+            .dateBetween(
+              date,
+              nextDate,
+              includeLower: true,
+              includeUpper: false,
+            )
+            .category((q) => q.typeEqualTo(CategoryType.income))
+            .amountProperty()
+            .sum(),
+        totalExpense: await isar.transactionModels
+            .filter()
+            .wallet((q) => q.idEqualTo(walletId))
+            .dateBetween(
+              date,
+              nextDate,
+              includeLower: true,
+              includeUpper: false,
+            )
+            .category((q) => q.typeEqualTo(CategoryType.expense))
+            .amountProperty()
+            .sum(),
+      );
+    }
+
+    return null;
   }
+}
 
-  return null;
-});
+final chartDetailDataProvider =
+    AsyncNotifierProvider<ChartDetailDataNotifier, ChartDetailData?>(
+  ChartDetailDataNotifier.new,
+);
