ab2c0f0bdc0b47a85b679099ce54dd2f95f33762
Rizal Anggoro <gnoogler4@gmail.com>
2024-02-16 15:10:02 +0700

    del: unnecessary code

--- lib/shared/enums/category_filter.dart
+++
@@ -1,9 +0,0 @@
-@Deprecated('Gunakan category type')
-enum CategoryFilter { all, income, expense }
-
-@Deprecated('Gunakan category type')
-extension XCategoryFilter on CategoryFilter {
-  bool get isAll => this == CategoryFilter.all;
-  bool get isIncome => this == CategoryFilter.income;
-  bool get isExpense => this == CategoryFilter.expense;
-}
