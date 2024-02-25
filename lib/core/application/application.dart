69f2b922557451252c25cd8e77a1024f5f3afc89
Rizal Anggoro <gnoogler4@gmail.com>
2024-02-25 16:06:35 +0700

    refactor: remove unnecessary comments

--- lib/core/application/application.dart
+++ lib/core/application/application.dart
@@ -22,10 +22,6 @@ class MyApplication extends ConsumerWidget {
             ? Brightness.dark
             : Brightness.light,
         colorSchemeSeed: const Color(0xff164863),
-        // colorScheme: ColorScheme.light(),
-        // colorScheme: ref.watch(themeProvider) == ThemeMode.dark
-        //     ? darkDynamic
-        //     : lightDynamic,
       ).copyWith(
         // app bar
         appBarTheme: const AppBarTheme(
@@ -36,70 +32,6 @@ class MyApplication extends ConsumerWidget {
         inputDecorationTheme: const InputDecorationTheme(
           filled: true,
         ),
-        // inputDecorationTheme: InputDecorationTheme(
-        //   contentPadding: Constants.inputPadding,
-        //   enabledBorder: OutlineInputBorder(
-        //     borderRadius: BorderRadius.all(Constants.inputRadius),
-        //     borderSide: BorderSide(
-        //       width: Constants.inputBorderWidth,
-        //       color: themeData.colorScheme.outline.withOpacity(.32),
-        //     ),
-        //   ),
-        //   focusedBorder: OutlineInputBorder(
-        //     borderRadius: BorderRadius.all(Constants.inputRadius),
-        //     borderSide: BorderSide(
-        //       width: Constants.inputBorderWidth,
-        //       color: themeData.colorScheme.primary,
-        //     ),
-        //   ),
-        // ),
-
-        // segmented button
-        // segmentedButtonTheme: SegmentedButtonThemeData(
-        //   selectedIcon: const Icon(Icons.check_rounded),
-        //   style: OutlinedButton.styleFrom(
-        //     shape: RoundedRectangleBorder(
-        //       // borderRadius: BorderRadius.all(Constants.buttonRadius),
-        //       borderRadius: BorderRadius.circular(24),
-        //     ),
-        //     side: BorderSide(
-        //       color: themeData.colorScheme.outline.withOpacity(.32),
-        //     ),
-        //   ),
-        // ),
-
-        // buttons
-        // filledButtonTheme: FilledButtonThemeData(
-        //   style: FilledButton.styleFrom(
-        //     shape: RoundedRectangleBorder(
-        //       borderRadius: BorderRadius.all(Constants.buttonRadius),
-        //     ),
-        //   ),
-        // ),
-        // outlinedButtonTheme: OutlinedButtonThemeData(
-        //   style: OutlinedButton.styleFrom(
-        //     side: BorderSide(
-        //       color: themeData.colorScheme.outline.withOpacity(.32),
-        //     ),
-        //     shape: RoundedRectangleBorder(
-        //       borderRadius: BorderRadius.all(Constants.buttonRadius),
-        //     ),
-        //   ),
-        // ),
-
-        // fab
-        // floatingActionButtonTheme: FloatingActionButtonThemeData(
-        //   elevation: 0,
-        //   backgroundColor: themeData.colorScheme.primary,
-        //   foregroundColor: themeData.colorScheme.onPrimary,
-        // ),
-
-        // navigation bar
-        // navigationBarTheme: NavigationBarThemeData(
-        //   backgroundColor: themeData.colorScheme.background,
-        //   surfaceTintColor: themeData.colorScheme.background,
-        //   labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-        // ),
       ),
     );
   }
