c7dde80d545a4aeae2a4008f550c5bf5037488a7
Rizal Anggoro <gnoogler4@gmail.com>
2024-02-25 17:36:24 +0700

    refactor: home page entities

--- lib/presentation/pages/home/page.dart
+++ lib/presentation/pages/home/page.dart
@@ -7,7 +7,7 @@ import '../../../core/router/router.gr.dart';
 import '../../../core/utils/extensions.dart';
 import '../../providers/selected_wallet.dart';
 import '../../widgets/bottom_sheets/transaction_filter.dart';
-import 'models/navigation_item.dart';
+import 'entities/navigation_item.dart';
 
 final _navigationItems = <NavigationItem>[
   NavigationItem(
@@ -43,77 +43,78 @@ class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => AutoTabsRouter(
-        routes: _routes,
-        builder: (context, child) {
-          final tabsRouter = AutoTabsRouter.of(context);
-          final activeIndex = tabsRouter.activeIndex;
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AutoTabsRouter(
+      routes: _routes,
+      builder: (context, child) {
+        final tabsRouter = AutoTabsRouter.of(context);
+        final activeIndex = tabsRouter.activeIndex;
 
-          return Scaffold(
-            appBar: AppBar(
-              toolbarHeight: 64,
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _navigationItems[activeIndex].title,
-                  ),
-                  if (activeIndex < 3) const Gap(2),
-                  if (activeIndex < 3)
-                    ref.watch(selectedWalletProvider).maybeWhen(
-                        data: (data) => Text(
-                              data?.name ?? 'Tidak ada dompet',
-                              style: context.textTheme.bodyMedium,
-                            ),
-                        orElse: () => const SizedBox())
-                ],
-              ),
-              actions: activeIndex < 3
-                  ? [
-                      if (activeIndex > 0)
-                        IconButton(
-                          onPressed: () => showModalBottomSheet(
-                            context: context,
-                            isScrollControlled: true,
-                            useSafeArea: true,
-                            showDragHandle: true,
-                            builder: (context) =>
-                                const BottomSheetTransactionFilter(),
+        return Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 64,
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  _navigationItems[activeIndex].title,
+                ),
+                if (activeIndex < 3) const Gap(2),
+                if (activeIndex < 3)
+                  ref.watch(selectedWalletProvider).maybeWhen(
+                      data: (data) => Text(
+                            data?.name ?? 'Tidak ada dompet',
+                            style: context.textTheme.bodyMedium,
                           ),
-                          icon: const Icon(Icons.sort_rounded),
-                        ),
+                      orElse: () => const SizedBox())
+              ],
+            ),
+            actions: activeIndex < 3
+                ? [
+                    if (activeIndex > 0)
                       IconButton(
-                        icon: const Icon(Icons.wallet_rounded),
-                        onPressed: () => context.router.push(
-                          const SelectWalletRoute(),
+                        onPressed: () => showModalBottomSheet(
+                          context: context,
+                          isScrollControlled: true,
+                          useSafeArea: true,
+                          showDragHandle: true,
+                          builder: (context) =>
+                              const BottomSheetTransactionFilter(),
                         ),
+                        icon: const Icon(Icons.sort_rounded),
                       ),
-                      const Gap(16),
-                    ]
-                  : null,
-            ),
-            bottomNavigationBar: NavigationBar(
-              selectedIndex: activeIndex,
-              onDestinationSelected: (value) =>
-                  tabsRouter.setActiveIndex(value),
-              destinations: _navigationItems
-                  .map((e) => NavigationDestination(
-                        icon: Icon(e.icon),
-                        selectedIcon: Icon(e.selectedIcon),
-                        label: e.title,
-                      ))
-                  .toList(),
-            ),
-            floatingActionButton: activeIndex < 3
-                ? FloatingActionButton(
-                    child: const Icon(Icons.add_rounded),
-                    onPressed: () => context.router.push(
-                      WriteTransactionRoute(),
+                    IconButton(
+                      icon: const Icon(Icons.wallet_rounded),
+                      onPressed: () => context.router.push(
+                        const SelectWalletRoute(),
+                      ),
                     ),
-                  )
+                    const Gap(16),
+                  ]
                 : null,
-            body: child,
-          );
-        },
-      );
+          ),
+          bottomNavigationBar: NavigationBar(
+            selectedIndex: activeIndex,
+            onDestinationSelected: (value) => tabsRouter.setActiveIndex(value),
+            destinations: _navigationItems
+                .map((e) => NavigationDestination(
+                      icon: Icon(e.icon),
+                      selectedIcon: Icon(e.selectedIcon),
+                      label: e.title,
+                    ))
+                .toList(),
+          ),
+          floatingActionButton: activeIndex < 3
+              ? FloatingActionButton(
+                  child: const Icon(Icons.add_rounded),
+                  onPressed: () => context.router.push(
+                    WriteTransactionRoute(),
+                  ),
+                )
+              : null,
+          body: child,
+        );
+      },
+    );
+  }
 }
